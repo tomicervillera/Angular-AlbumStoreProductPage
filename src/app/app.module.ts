4f423a73d67eb1d18786cfb4745f981b44cdb544
tcervill <tcervillera@lasegunda.com.ar>
2022-04-12 09:28:16 -0300

    fix m7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,11 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
-let appRoutes: Routes =
-  [
-    { path: 'products', component: ProductListComponent },
-    { path: 'product/:id', component: ProductPageComponent },
-    { path: '', redirectTo: 'products', pathMatch: 'full' }
-  ];
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
+]
 @NgModule({
   declarations: [
     AppComponent,
